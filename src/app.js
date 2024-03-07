667a4a343926fe6cf598311519ab155b036a2a07
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-03-07 17:40:23 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".es", ".com", ".net"];
+for (let pronouns in pronoun) {
+  for (let adjs in adj) {
+    for (let nouns in noun) {
+      for (let extensions in extension) {
+        console.log(pronoun[pronouns] + adj[adjs] + noun[nouns] + extension[extensions]
+        );
+      }
+    }
+  }
+}
